48184e35d272ffcb2427c223a0d44e6e85c6b53f
Rafael <rafaelsaibro1@gmail.com>
2023-06-07 15:06:25 -0300

    att

---
+++ src/variaveis/fluxo/ExemploTabuadaDOWhile.java
@@ -0,0 +1,20 @@
+package variaveis.fluxo;
+import java.util.Scanner;
+public class ExemploTabuadaDOWhile {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+
+        String resposta = " ";
+        do {
+            System.out.println("Digite um número para gerar a tabuada: ");
+            int num = s.nextInt();
+            for (int i = 0; i<=10;i++) {
+                System.out.println(num + " X " + i + " = " + num*i);
+            }
+            System.out.println("Deseja gerar novamente a tabuada de algum número? ");
+            resposta = s.next();
+        } while (resposta.equalsIgnoreCase("Sim") || resposta.equalsIgnoreCase("s") || resposta.equalsIgnoreCase("desejo"));
+
+        System.out.println("Obrigado. ");
+    }
+}
---
+++ src/variaveis/fluxo/ExemploWhile.java
@@ -0,0 +1,11 @@
+package variaveis.fluxo;
+
+public class ExemploWhile {
+    public static void main(String args[]){
+        int count = 0;
+        while (count <= 1000) {
+            System.out.println("Repetição nr: " + count);
+            count++;
+        }
+    }
+}
