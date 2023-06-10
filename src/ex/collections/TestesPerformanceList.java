7142122be38c2b4c4054804627e9772ff3b5ebb5
Rafael <rafaelsaibro1@gmail.com>
2023-06-09 22:11:46 -0300

    att

---
+++ src/ex/collections/TestesPerformanceList.java
@@ -0,0 +1,80 @@
+package ex.collections;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class TestesPerformanceList {
+    public static void main(String[] args){
+        testeArrayListAdd();
+        testeLinkedListAdd();
+
+        testeArrayListRemove();
+        testeLinkedListRemove();
+
+    }
+    private static void testeLinkedListRemove(){
+        final int MAX = 2000;
+        long tInicio = System.currentTimeMillis();
+        List<Integer> lista = new LinkedList<>();
+
+        for (int i = 0; i < MAX; i++) {
+            lista.add(i);
+        }
+
+        for (int i = MAX -1; i > 0; i--){
+            lista.remove(i);
+        }
+
+        long tFim = System.currentTimeMillis();
+        System.out.println("tempo total LinkedListRemove: " + (tFim - tInicio));
+    }
+    private static void testeLinkedListAdd(){
+        final int MAX = 2000;
+        long tInicio = System.currentTimeMillis();
+        List<Integer> lista = new LinkedList<>();
+
+        for (int i = 0; i < MAX; i++){
+            lista.add(i);
+        }
+
+        for (int i = 0; i < MAX; i++){
+            lista.contains(i);
+        }
+
+        long tFim = System.currentTimeMillis();
+        System.out.println("Tempo total LinkedList ADD: " + (tFim + tInicio));
+    }
+    private static void testeArrayListRemove(){
+        final int MAX = 2000;
+        long tInicio = System.currentTimeMillis();
+        List<Integer> lista = new LinkedList<>();
+
+        for (int i = 0; i < MAX; i++){
+            lista.add(i);
+        }
+
+        for (int i = MAX -1; i > 0; i--){
+            lista.remove(i);
+        }
+
+        long tFim = System.currentTimeMillis();
+        System.out.println("Tempo total ArrayListRemove : " + (tFim + tInicio));
+    }
+    private static void testeArrayListAdd(){
+        final int MAX = 2000;
+        long tInicio = System.currentTimeMillis();
+        List<Integer> lista = new ArrayList<>();
+
+        for (int i = 0; i < MAX; i++){
+            lista.add(i);
+        }
+
+        for (int i = 0; i < MAX; i++){
+            lista.contains(i);
+        }
+
+        long tFim = System.currentTimeMillis();
+        System.out.println("Tempo total ArrayList ADD: " + (tFim + tInicio));
+    }
+}
