82f22cd4bfea5893b3ad433bab0d2d55bc5d14ca
Rafael <rafaelsaibro1@gmail.com>
2023-06-13 19:40:06 -0300

    att

--- src/br/set/ExemploHashSetAluno.java
+++ src/br/set/ExemploHashSetAluno.java
@@ -8,7 +8,33 @@ public class ExemploHashSetAluno {
     public static void main(String args[]){
         exemploListaSimples();
         exemploConsultando();
+        exemploRemover();
 
+    }
+
+    private static void exemploRemover() {
+        System.out.println("***exemploRemover***");
+        Set<Aluno> conjunto = new HashSet<Aluno>();
+
+        Aluno a = new Aluno("Joao da Silva", "Linux Básico",0);
+        Aluno e = new Aluno("Joao da Silva", "Linux Básico",0);
+        Aluno b = new Aluno("Antonio Sousa", "Open Office",0);
+        Aluno c = new Aluno("Lúcia Ferreira", "Internet",0);
+        Aluno d = new Aluno("Antonio Sousa", "Open Office",0);
+
+        conjunto.add(a);
+        conjunto.add(b);
+        conjunto.add(c);
+        conjunto.add(d);
+
+        conjunto.remove(c);
+        System.out.println(conjunto);
+        System.out.println("");
+
+        for (Aluno aluno : conjunto){
+            System.out.println(aluno);
+
+        }
     }
 
     private static void exemploConsultando() {
@@ -45,5 +71,6 @@ public class ExemploHashSetAluno {
         conjunto.add(d);
         conjunto.add(e);
         System.out.println(conjunto);
+        System.out.println("");
     }
 }
